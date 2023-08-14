65bcc555f1d24480466364c8f297ae574003a497
shwatanap <watanabeshou1520@gmail.com>
2023-08-14 22:14:13 +0900

    :zap: template routerを作成 #6

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,35 +1,13 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Router() *gin.Engine {
 	router := gin.Default()
-	router.LoadHTMLGlob("internal/view/*.html")
+	initTemplateRouter(router)
 	initHealthRouter(router)
-	initAuthRouter(router)
-
-	router.GET("/", func(c *gin.Context) {
-		// code := c.Query("code")
-		// g, err := oauth.NewGoogle()
-		// if err != nil {
-		// 	// TODO: error handling
-		// 	panic(err)
-		// }
-
-		// client, err := g.GetClient(code)
-		// if err != nil {
-		// 	// TODO: error handling
-		// 	panic(err)
-		// }
-
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
-	router.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", gin.H{})
-	})
+	initOAuthRouter(router)
 	return router
 }
---
+++ internal/router/template.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initTemplateRouter(r *gin.Engine) {
+	r.LoadHTMLGlob("internal/view/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	r.GET("/signup", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "signup.html", gin.H{})
+	})
+}
