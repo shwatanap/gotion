2fcddf5d79e854790e0b47857e22648d9b25ec4d
shwatanap <watanabeshou1520@gmail.com>
2023-08-15 19:37:36 +0900

    :sparkles: Calendar Model実装 #4

---
+++ internal/model/calendar.go
@@ -0,0 +1,29 @@
+package model
+
+import "google.golang.org/api/calendar/v3"
+
+type Calendar struct {
+	Calendar *calendar.CalendarListEntry
+}
+
+type CalendarsService struct {
+	Srv *calendar.Service
+}
+
+func NewCalendarService(srv *calendar.Service) *CalendarsService {
+	return &CalendarsService{
+		Srv: srv,
+	}
+}
+
+func (cs *CalendarsService) List(timeMin string) ([]*Calendar, error) {
+	calendarList, err := cs.Srv.CalendarList.List().Do()
+	if err != nil {
+		return nil, err
+	}
+	cms := make([]*Calendar, len(calendarList.Items))
+	for i, calendarListEntry := range calendarList.Items {
+		cms[i] = &Calendar{Calendar: calendarListEntry}
+	}
+	return cms, nil
+}
