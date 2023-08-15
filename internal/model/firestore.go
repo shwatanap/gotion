4d1d15ccb0e098c5ba8f23645a0d5cd2b993fdcb
shwatanap <watanabeshou1520@gmail.com>
2023-08-16 08:30:22 +0900

    :sparkles: firestoreを操作するコード実装 #10

---
+++ internal/model/firestore.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+type Firestore struct {
+	Client *firestore.Client
+}
+
+func NewFirestore(ctx context.Context) *firestore.Client {
+	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return client
+}
