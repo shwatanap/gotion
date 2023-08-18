cf107d229755d02e1359414e98cf117025b1fb08
Shou Watanabe <76590212+shwatanap@users.noreply.github.com>
2023-08-18 19:40:39 +0900

    :sparkles: notionのイベントを作成する機能実装 #17 (#19)

--- internal/model/notion.go
+++ internal/model/notion.go
@@ -7,12 +7,12 @@ import (
 )
 
 type NotionClient struct {
-	Client *notionapi.Client
+	client *notionapi.Client
 }
 
 func NewNotionClient(apikey string) *NotionClient {
 	client := notionapi.NewClient(notionapi.Token(apikey))
-	return &NotionClient{Client: client}
+	return &NotionClient{client: client}
 }
 
 type CreateDatabaseRequest struct {
@@ -20,7 +20,7 @@ type CreateDatabaseRequest struct {
 	Title  string
 }
 
-func (nc *NotionClient) CreateDatabase(ctx context.Context, req CreateDatabaseRequest) error {
+func (nc *NotionClient) CreateDatabase(ctx context.Context, req CreateDatabaseRequest) (*notionapi.Database, error) {
 	request := &notionapi.DatabaseCreateRequest{
 		Parent: notionapi.Parent{
 			Type:      notionapi.ParentTypePageID,
@@ -68,6 +68,88 @@ func (nc *NotionClient) CreateDatabase(ctx context.Context, req CreateDatabaseRe
 		},
 		IsInline: true,
 	}
-	_, err := nc.Client.Database.Create(ctx, request)
+	res, err := nc.client.Database.Create(ctx, request)
+	return res, err
+}
+
+type AddEventRequest struct {
+	DatabaseID       notionapi.DatabaseID
+	Title            string
+	SelectedCalendar notionapi.Option
+	DateStart        *notionapi.Date
+	DateEnd          *notionapi.Date
+}
+
+func (nc *NotionClient) AddEvent(ctx context.Context, req AddEventRequest) error {
+	_, err := nc.client.Page.Create(ctx, &notionapi.PageCreateRequest{
+		Parent: notionapi.Parent{
+			Type:       notionapi.ParentTypeDatabaseID,
+			DatabaseID: req.DatabaseID,
+			Workspace:  false,
+		},
+		Properties: map[string]notionapi.Property{
+			"name": &notionapi.TitleProperty{
+				ID:   "title",
+				Type: notionapi.PropertyTypeTitle,
+				Title: []notionapi.RichText{
+					{
+						Type:     notionapi.ObjectTypeText,
+						Text:     &notionapi.Text{Content: req.Title},
+						Mention:  nil,
+						Equation: nil,
+					},
+				},
+			},
+			"calendar": &notionapi.SelectProperty{
+				ID:     "calendar",
+				Type:   notionapi.PropertyTypeSelect,
+				Select: req.SelectedCalendar,
+			},
+			"date": &notionapi.DateProperty{
+				ID:   "date",
+				Type: notionapi.PropertyTypeDate,
+				Date: &notionapi.DateObject{
+					Start: req.DateStart,
+					End:   req.DateEnd,
+				},
+			},
+		},
+	})
 	return err
 }
+
+// func (nc *NotionClient) GetDatabase(ctx context.Context, databaseId notionapi.DatabaseID) error {
+// 	res, err := nc.client.Database.Query(ctx, databaseId, nil)
+// 	if err != nil {
+// 		log.Println("😡", err.Error())
+// 	}
+// 	for _, page := range res.Results {
+// 		log.Println("=====================================")
+// 		// log.Printf("🥺: %v, %v, %v", page.Properties["calendar"], page.Properties["date"], page.Properties["name"])
+// 		log.Printf("🥺:%v", page.Properties["名前"])
+// 		// log.Printf("🥺:%v", page.Properties["name"])
+// 		// log.Printf("🥺:%v", page.Properties["date"].(*notionapi.DateProperty).Date)
+// 		// log.Printf("👏: %v", page.Properties["calendar"])
+// 		// log.Printf("🥺: %v", page)
+// 		// log.Printf("🥺: %v", page)
+// 		// log.Printf("🥺: %v", page)
+// 	}
+// 	log.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+// 	db, _ := nc.client.Database.Get(ctx, databaseId)
+// 	for key, value := range db.Properties {
+// 		log.Printf("%s: %v", key, value)
+// 	}
+// 	return nil
+// }
+
+// func (nc *NotionClient) GetPage(ctx context.Context) {
+// 	page, err := nc.client.Page.Get(ctx, "34b55e9279764a4d9fc153657446631b")
+// 	if err != nil {
+// 		log.Println("😡", err.Error())
+// 	}
+// 	log.Printf("🥺: %v", page.Parent)
+// 	log.Printf("🥺: %v", page.Properties)
+// 	// log.Printf("🥺: %v", page.Properties["date"])
+// 	log.Printf("🥺: %v", page.Properties["date"].(*notionapi.DateProperty).Date)
+// 	log.Printf("🥺: %v", page.Properties["calendar"])
+// }
