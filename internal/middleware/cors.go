2ca4d06080e5de601354095644656b6966a70c18
shwatanap <watanabeshou1520@gmail.com>
2023-09-12 11:29:42 +0900

    :sparkles: CORS設定 #30

---
+++ internal/middleware/cors.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Cors(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173",
+		},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"X-CSRF-Token",
+			"Authorization",
+			"withCredentials",
+		},
+		ExposeHeaders: []string{
+			"Location",
+		},
+		MaxAge:           24 * time.Hour,
+		AllowCredentials: true,
+	}))
+}
